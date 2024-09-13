97243b751e8d2e3cd80863945e749cc2f7180af6
Matafon <79162313+Matafon@users.noreply.github.com>
2024-09-13 20:34:31 +0200

    Add files via upload

---
+++ Uloha_2_detekce_pismen/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Napiš znak.");
+
+char znak = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+if (char.IsLower(znak))
+{
+    Console.WriteLine($"{znak} patří mezi malá písmena.");
+}
+else if (char.IsUpper(znak))
+{
+    Console.WriteLine($"{znak} patří mezi velká písmena.");
+}
+else if (char.IsDigit(znak))
+{
+    Console.WriteLine($"{znak} je číslice.");
+}
+else if (znak == '!')
+{
+    Console.WriteLine("Vykřičník");
+}
+else if (znak == '?')
+{
+    Console.WriteLine("Otazník");
+}
+else if (znak == '*')
+{
+    Console.WriteLine("Hvězdička");
+}
+else if (znak == '@')
+{
+    Console.WriteLine("Zavináč");
+}
+else if (znak == '#')
+{
+    Console.WriteLine("křížek");
+}
+else
+{
+    Console.WriteLine("Jiný znak.");
+}
